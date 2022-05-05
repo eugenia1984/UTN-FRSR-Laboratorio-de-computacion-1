3de766834f6fdd108f83d366f7a3e7c969fc2855
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2022-05-04 22:34:08 -0300

    agrego un nuevo ejercicio

--- clase01_02_03_04_05/README.md
+++ clase01_02_03_04_05/README.md
@@ -648,5 +648,22 @@ else:
   print("No esta dentro del rango")
 ```
 
+## :star: Ejercicio 3: El mayor de dos numeros
+
+Solicitar al usuario dos valores numero1 y numero2 de tipo int
+
+Se debe imprimir el mayor de los dos numeros y que se muestre: El mayor es:
+
+```Python
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese otro numero: "))
+if numero1 > numero2:
+  print(f"El número mayor es: {numero1}")
+elif numero2 > numero1:
+  print(f"El número mayor es {numero2}")
+else:
+  print("Los dos numeros ingresados {numero1} y {numero2} son iguales")
+```
+
 ---
 ---
--- clase5.py
+++ clase5.py
@@ -27,3 +27,15 @@ if veinte or treinta:
   print("Esta dentro del rango de los 20`s a 30's años")
 else:
   print("No esta dentro del rango")
+
+# Ejercicio 3: El mayor de dos numeros
+# Solicitar al usuario dos valores numero1 y numero2 de tipo int
+# Se debe imprimir el mayor de los dos numeros y que se muestre: El mayor es:
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese otro numero: "))
+if numero1 > numero2:
+  print(f"El número mayor es: {numero1}")
+elif numero2 > numero1:
+  print(f"El número mayor es {numero2}")
+else:
+  print("Los dos numeros ingresados {numero1} y {numero2} son iguales")
