415542e7f91ecdf24f5be1a0b62e3c21772f6c85
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2022-05-11 22:18:30 -0300

    agrego un segundo ejercicio para practica if elseif else

--- clase01_02_03_04_05_06/README.md
+++ clase01_02_03_04_05_06/README.md
@@ -746,4 +746,24 @@ Si condicion = '' -> se imprime Condicion sn especificar
 Si condicion = 20 -> se imprime Condicion sn especificar
 
 ---
+
+## Segundo ejercicio para practicar IF - ELIF - ELSE
+
+```Python
+num = int(input("Ingrese un numero en el rango de a al 3: "))
+numTexto = ''
+
+if num == 1:
+  numTexto = 'Numero uno'
+elif num == 2:
+  numTexto = 'Numero dos'
+elif num == 3:
+  numTexto = 'Numero 3'
+else:
+  numTexto = 'Has ingresado un número fuera de rango'
+
+print(f'El número ingresado es: {num} - {numTexto}')
+```
+
+---
 ---
--- clase6.py
+++ clase6.py
@@ -13,3 +13,18 @@ else:
 # Si condicion = 1 -> se imprime Condicion verdadera
 # Si condicion = '' -> se imprime Condicion sn especificar
 # Si condicion = 20 -> se imprime Condicion sn especificar
+
+# Segundo ejercicio para practicar IF - ELIF - ELSE
+num = int(input("Ingrese un numero en el rango de a al 3: "))
+numTexto = ''
+
+if num == 1:
+  numTexto = 'Numero uno'
+elif num == 2:
+  numTexto = 'Numero dos'
+elif num == 3:
+  numTexto = 'Numero 3'
+else:
+  numTexto = 'Has ingresado un número fuera de rango'
+
+print(f'El número ingresado es: {num} - {numTexto}')
